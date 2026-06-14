a6b818aec8726bd68b519a190f88215b815ed35a
Hafsa Imran <hafsaemran1712@gmail.com>
2026-06-15 00:31:01 +0500

    Update

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ let onBtn = document.querySelector("#on-btn")
 let offBtn = document.querySelector("#off-btn")
 
 onBtn.addEventListener("click", () => {
-  bulb.src = "images/bulb on.png";
+  bulb.src = "images/bulb-on.png";
 });
 
 offBtn.addEventListener("click", () => {
-  bulb.src = "images/bulb off.png";
+  bulb.src = "images/bulb.png";
 });
